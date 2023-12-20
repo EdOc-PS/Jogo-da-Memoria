b9dbd1e53fdceb00856ac82cb32c6b81f5721f44
Eduardo Octávio <eeuardooctavio@gmail.com>
2023-12-20 12:04:16 -0300

    1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,35 @@ const emojis = [
     "👾"
 ];
 let openCards = [];
-let randomEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1 ));
+let randomEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "itens";
     box.innerHTML = randomEmojis[i];
-document.querySelector(".game").appendChild(box);
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if (openCards.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Voce Venceu");
+    }
 }
